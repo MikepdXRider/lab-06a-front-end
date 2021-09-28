dcac219fbacef9520901f224bb30675e21e59b43
Michael Rider <mikepdxrider@gmail.com>
2021-09-28 15:09:59 -0700

    adds react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
-import ListPage from './components/ListPage.js';
+import React from "react";
+import ListPage from "./components/ListPage.js";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <>
-    <ListPage />
-    </>
-  );
+// Params are placeholders in the URL that begin
+// with a colon, like the `:id` param defined in
+// the route in this example. A similar convention
+// is used for matching dynamic segments in other
+// popular web frameworks like Rails and Express.
+
+export default class ParamsExample extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <h2>Header</h2>
+          <Switch>
+            <Route 
+              path="/" 
+              exact
+              render={(routerProps) => <ListPage {...routerProps} />} 
+            />          
+            <Route 
+              path="/:id" 
+              exact
+              render={(routerProps) => <DetailsPage {...routerProps} />} 
+            />          
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/DetailsPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DetailsPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
