08bbc63fb36d581e44f31a0242ea4f2d7f61e544
Michael Rider <mikepdxrider@gmail.com>
2021-10-05 14:15:06 -0700

    resolves a function error in fetch-utils.js

--- src/fetch-utils.js
+++ src/fetch-utils.js
@@ -4,5 +4,5 @@ import request from 'superagent';
 export default async function getTeas() {
     const response = await request.get(`https://lab06b-be.herokuapp.com/teas`);
 
-    this.setState({data: response.body});
+    return response.body;
 }
